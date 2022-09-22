b24e731df31536d9f9de4534e4c6535aa8c3694f
liamdono <donovali@mail.uc.edu>
2022-09-22 15:22:38 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main
+{
+    public static void main (String[] args)
+    {
+
+        String partyLetter = "D";
+
+        if (partyLetter == "D")
+
+        {
+            System.out.println("You get a democratic donkey!");
+        }
+
+        else if (partyLetter == "R")
+
+        {
+            System.out.println("You get a republican elephant!");
+        }
+
+        else if (partyLetter == "I")
+        {
+            System.out.println("You are independent!");
+
+        }
+        else
+            System.out.println("You are with another party!");
+    }
+}
